ac1a3f28468910037efc7516612ca5a009d8622c
harshanth0112 <harshanth.ai22@krct.ac.in>
2026-04-22 19:49:30 +0530

    Update CORS to allow GitHub Pages origin

--- main.py
+++ main.py
@@ -18,7 +18,12 @@ app = FastAPI(title="Personal Library API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://harshanth0112.github.io"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
